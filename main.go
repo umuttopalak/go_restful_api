d3c7f04f904782ae8234e58347193136c3aa37f4
umuttopalak <umuttopalak@hotmail.com>
2023-12-22 01:01:03 +0300

    user routing added

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"example/restfulapi/albums"
 	"example/restfulapi/todos"
+	"example/restfulapi/users"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,17 +11,25 @@ import (
 func main() {
 	router := gin.Default()
 
+	//Albums
 	router.GET("/albums", albums.GetAlbums)
 	router.GET("/albums/album/:id", albums.GetAlbumByID)
 	router.GET("/albums/:artist", albums.GetAlbumsByArtist)
 	router.POST("/albums", albums.PostAlbums)
 	router.DELETE("/albums/:id", albums.DeleteAlbum)
 
+	//Todos
 	router.POST("/todos", todos.PostTodo)
 	router.GET("/todos", todos.GetTodos)
 	router.GET("/todos/todo/:id", todos.GetTodoById)
 	router.GET("/todos/:author", todos.GetTodoByAuthor)
 	router.DELETE("/todos/:id", todos.DeleteTodo)
+
+	//Users
+	router.POST("/users", users.PostUser)
+	router.GET("/users", users.GetUsers)
+	router.GET("/users/user/:id", users.GetUser)
+	router.DELETE("/users/user/:id", users.DeleteUser)
 
 	router.Run("localhost:8080")
 }
