370d0adc5b7799f3aaa10c9a79c83038634e7e9e
umuttopalak <umuttopalak@hotmail.com>
2023-12-21 15:38:37 +0300

    delete routers added

--- main.go
+++ main.go
@@ -14,11 +14,13 @@ func main() {
 	router.GET("/albums/album/:id", albums.GetAlbumByID)
 	router.GET("/albums/:artist", albums.GetAlbumsByArtist)
 	router.POST("/albums", albums.PostAlbums)
+	router.DELETE("/albums/:id", albums.DeleteAlbum)
 
 	router.POST("/todos", todos.PostTodo)
 	router.GET("/todos", todos.GetTodos)
 	router.GET("/todos/todo/:id", todos.GetTodoById)
 	router.GET("/todos/:author", todos.GetTodoByAuthor)
+	router.DELETE("/todos/:id", todos.DeleteTodo)
 
 	router.Run("localhost:8080")
 }
